33a6a863d8d6b520274008900a3d99f9fbed3ef7
TusharCitrusbug <98886632+TusharCitrusbug@users.noreply.github.com>
2022-05-18 10:58:35 +0530

    added settings

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*n_ftxd!_d=e%xfys$9e%j#@24c37oss2n+gd9x8ju=52+@c=h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
